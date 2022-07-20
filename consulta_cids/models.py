e9835ba6061ab88c8cd63fb4a8e7a9d54bac847b
Jailton Lima <jailtonlima1001@gmail.com>
2022-07-20 16:31:14 -0300

    Definindo uma classe para listar os grupos de doenca CID10

--- consulta_cids/models.py
+++ consulta_cids/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
-class Doencacategoria(models.Model):
+class DoencaCategoria(models.Model):
     """Definindo categoria e descricao de uma doença com CID10"""
     categoria = models.CharField(max_length=4)
+    descricao = models.TextField()
+
+
+class DoencaGrupo(models.Model):
+    """Definindo categoriainicial e final para os grupos de doença com CID10"""
+    categoria_inicial = models.CharField(max_length=4)
+    categoria_final = models.CharField(max_length=4)
     descricao = models.TextField()
