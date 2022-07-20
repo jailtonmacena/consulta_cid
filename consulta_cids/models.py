e93045e2e2d6b1e7977259771d9cabd212b4d7d7
Jailton Lima <jailtonlima1001@gmail.com>
2022-07-20 20:14:58 -0300

    Definindo funções para retornar uma representação em string dos atributos categoria e categoria_inical

--- consulta_cids/models.py
+++ consulta_cids/models.py
@@ -7,8 +7,18 @@ class DoencaCategoria(models.Model):
     descricao = models.TextField()
 
 
+    def __str__(self):
+        """Retornar uma representação em string do atributo categoria"""
+        return self.categoria
+
+
 class DoencaGrupo(models.Model):
     """Definindo categoriainicial e final para os grupos de doença com CID10"""
     categoria_inicial = models.CharField(max_length=4)
     categoria_final = models.CharField(max_length=4)
     descricao = models.TextField()
+
+
+    def __str__(self):
+        """Retorna uma representação em string do atributo categoria_inicial"""
+        return self.categoria_inicial
