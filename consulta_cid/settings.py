beb97abe7a8a2169afe967d9e2cbbb5bebf9327c
Jailton Lima <jailtonlima1001@gmail.com>
2022-07-20 15:51:02 -0300

    Instalando app unaccent postgresql

--- consulta_cid/settings.py
+++ consulta_cid/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # unaccent postgrsql
+    'django.contrib.postgres' ,
 ]
 
 MIDDLEWARE = [
@@ -62,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
