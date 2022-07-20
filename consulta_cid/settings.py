c9abb78108e87632601eb8d29efe7ffe8231e1c7
Jailton Lima <jailtonlima1001@gmail.com>
2022-07-20 15:38:00 -0300

    Alterando o BD para postgresql

--- consulta_cid/settings.py
+++ consulta_cid/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'consulta_cid.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'consultacid',
+        'USER': 'postgres',
+        'PASSWORD': '98237135' ,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
