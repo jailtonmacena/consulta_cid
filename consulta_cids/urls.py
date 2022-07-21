b3a72c00dc0daa4d39ed733445b2184cc1b8633c
Jailton Lima <jailtonlima1001@gmail.com>
2022-07-21 12:31:00 -0300

    Adicionando path para a função lista_cids para listar todos os CIDS

--- consulta_cids/urls.py
+++ consulta_cids/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name = "home.html"),
     path('search_cid/', views.buscar_cids, name="search.html"),
+    path('list_cids/', views.lista_cids, name="list_cids")
 ]
