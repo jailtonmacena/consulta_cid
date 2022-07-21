150e90ff5378ba5b664186f0b017328b9718cdcf
Jailton Lima <jailtonlima1001@gmail.com>
2022-07-21 10:35:30 -0300

    Adicionando path para buscar_cids (search.html)

--- consulta_cids/urls.py
+++ consulta_cids/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = "home.html")
+    path('', views.home, name = "home.html"),
+    path('search_cid/', views.buscar_cids, name="search.html"),
 ]
