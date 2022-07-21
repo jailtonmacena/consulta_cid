66fdbb3ff49613d25a26d9ba9bd9526c541370e0
Jailton Lima <jailtonlima1001@gmail.com>
2022-07-21 13:22:25 -0300

    Adicionando path para list_cids e list_group

--- consulta_cids/urls.py
+++ consulta_cids/urls.py
@@ -1,8 +1,10 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name = "home.html"),
     path('search_cid/', views.buscar_cids, name="search.html"),
-    path('list_cids/', views.lista_cids, name="list_cids")
+    path('list_cids/', views.lista_cids, name="list_cids"),
+    path('list_group/', views.lista_grupo, name="list_group"),
 ]
